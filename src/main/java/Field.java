1e917e123c04f708497e6691eae73aaecae8ef17
Phoenix <liakhovetsmaksim@gmail.com>
2017-04-10 14:45:55 +0300

    Field

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -4,4 +4,32 @@ package main.java;
  * Created by Phoenix on 10.04.2017.
  */
 public class Field {
+    private char[][] board;
+    private char currentPlayer;
+
+    public Field(){
+        board = new char[3][3];
+        currentPlayer = 'x';
+        InitializeField();
+    }
+
+    public void InitializeField(){
+        for (int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+               board [i][j] = '-';
+            }
+        }
+    }
+    public void PrintBoard(){
+        System.out.println("----------");
+
+        for (int i = 0; i < 3; i++){
+            System.out.println("| ");
+            for (int j = 0; j < 3; j++){
+                System.out.println(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("------------");
+        }
+    }
 }
