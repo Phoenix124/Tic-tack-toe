622f5aad24326e8bfa9265adcbeecaa0b0f44076
Phoenix <liakhovetsmaksim@gmail.com>
2017-04-12 13:15:09 +0300

    Runner

--- src/main/java/Gamerunner.java
+++ src/main/java/Gamerunner.java
@@ -1,12 +1,16 @@
 package main.java;
 
+
+import java.util.Scanner;
+
 /**
  * Created by Phoenix on 10.04.2017.
  */
+
 public class Gamerunner {
     public static void main(String[] args) {
-            Field field = new Field();
-            field.printBoard();
-
+        GameLogic game = new GameLogic();
+        game.play();
     }
+
 }
