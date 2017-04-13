1894c0848d11f9c2eb2d68852f313ed2a008ba97
Phoenix <liakhovetsmaksim@gmail.com>
2017-04-13 13:45:56 +0300

    Logic

--- src/main/java/GameLogic.java
+++ src/main/java/GameLogic.java
@@ -1,6 +1,8 @@
 package main.java;
 
 
+import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -8,332 +10,184 @@ import java.util.Scanner;
  */
 public class GameLogic{
 
-    public static Scanner sc = new Scanner(System.in);
-    public static boolean WINNER_FOUND = false;
-    public static int current_player  = 0;
-    public static boolean game_winner = false;
-    public static boolean is_draw = false;
-    public static int board[][] = new int[3][3];
+    private char mBoard[] = {'1','2','3','4','5','6','7','8','9'};
+    private final int BOARD_SIZE = 9;
+
+    public static final char HUMAN_PLAYER = 'X';
+    public static final char COMPUTER_PLAYER = 'O';
+
+    private Random mRand;
+
+    Field field = new Field();
 
     public void play(){
 
-        current_player = 1;
-        printBoard();
-        do {
-            if (current_player == 1) {
-                promptInputandSet(current_player);
-            } else {
-                computerPromptInputandSet(current_player);
+                mRand = new Random();
+
+        char turn = HUMAN_PLAYER;
+        int  win = 0;
+
+        while (win == 0)
+        {
+            field.displayBoard();
+
+            if (turn == HUMAN_PLAYER)
+            {
+                getUserMove();
+                turn = COMPUTER_PLAYER;
             }
-            game_winner = checkWinner(current_player);
-            if (game_winner == true && is_draw == false) {
-                System.out.println("You Win " + current_player);
-                System.exit(0);
+            else
+            {
+                getComputerMove();
+                turn = HUMAN_PLAYER;
             }
-            if (is_draw == true) {
-                System.out.println("It's draw , play again");
-                System.exit(0);
-            }
-            if (current_player == 1) {
-                current_player = 2;
-            } else if (current_player == 3) {
-                current_player = 1;
-            }
-        }while (WINNER_FOUND != true);
+
+            win = checkForWinner();
         }
 
-    private void computerPromptInputandSet(int current_player2){
+        field.displayBoard();
 
-        System.out.println("Computer draw");
 
-        int possible_row = -1;
-        int possible_col = -1;
-        int col_correctness = 0;
-        int row_correctness = 0;
+        System.out.println();
+        if (win == 1)
+            System.out.println("It's a tie.");
+        else if (win == 2)
+            System.out.println(HUMAN_PLAYER + " wins!");
+        else if (win == 3)
+            System.out.println(COMPUTER_PLAYER + " wins!");
+        else
+            System.out.println("There is a logic problem!");
+    }
 
-        int opp_current_player = 1;
+    void getUserMove() {
 
-        for (int i = 0; i < 3; i++){
-            row_correctness = 0;
-            for (int j = 0; j < 3; j++){
-                if (board[i][j] != opp_current_player){
-                    possible_row = i;
-                    possible_col = j;
-                }else {
-                    row_correctness++;
+        Scanner s = new Scanner(System.in);
+
+        int move = -1;
+
+        while (move == -1) {
+            try {
+                System.out.print("Enter your move: ");
+                move = s.nextInt();
+
+                while (move < 1 || move > BOARD_SIZE ||
+                        mBoard[move-1] == HUMAN_PLAYER || mBoard[move-1] == COMPUTER_PLAYER) {
+
+                    if (move < 1 || move > BOARD_SIZE)
+                        System.out.println("Please enter a move between 1 and " + BOARD_SIZE + ".");
+                    else
+                        System.out.println("That space is occupied.  Please choose another space.");
+
+                    System.out.print("Enter your move: ");
+                    move = s.nextInt();
                 }
             }
-        if (row_correctness == 2 && board[possible_row][possible_col] != current_player2){
-
-                updateBoard(possible_row,possible_col,current_player2);
-                return;
-        }
-        }
-
-        possible_row = -1;
-        possible_col = -1;
-
-        for (int i = 0; i < 3; i++){
-            col_correctness = 0;
-            for (int j = 0; j < 3; j++){
-                if (board[j][i] != opp_current_player){
-                    possible_row = j;
-                    possible_col = i;
-                }else {
-                    col_correctness++;
-                }
-            }
-        if (col_correctness == 2 && board[possible_row][possible_col] != current_player2){
-
-                updateBoard(possible_row,possible_col,current_player2);
-                return;
-        }
-        }
-
-        int left_diagonal_count = 0;
-        if (board[0][0] == opp_current_player){
-            left_diagonal_count++;
-        }else {
-            possible_row = 0;
-            possible_col = 0;
-        }
-        if (board[1][1] == opp_current_player){
-            left_diagonal_count++;
-        }else {
-            possible_row = 1;
-            possible_col = 1;
-        }
-        if (board[2][2] == opp_current_player){
-            left_diagonal_count++;
-        }else {
-            possible_row = 2;
-            possible_col = 2;
-        }
-
-        if (left_diagonal_count == 2 && board[possible_row][possible_col] != current_player2){
-            updateBoard(possible_col,possible_col,current_player2);
-            return;
-        }
-
-        int right_diagonal_count = 0;
-        if (board[2][0] == opp_current_player){
-            right_diagonal_count++;
-        }else {
-            possible_row = 2;
-            possible_col = 0;
-        }
-        if (board[1][1] == opp_current_player){
-            right_diagonal_count++;
-        }else {
-            possible_row = 1;
-            possible_col = 1;
-        }
-        if (board[0][2] == opp_current_player){
-            right_diagonal_count++;
-        }else {
-            possible_row = 0;
-            possible_col = 2;
-        }
-
-        if (right_diagonal_count == 2 && board[possible_row][possible_col] != current_player2){
-            updateBoard(possible_col,possible_col,current_player2);
-            return;
-        }
-
-        row_correctness = 0;
-
-        for (int i = 0; i< 3; i++){
-            for (int j = 0; j < 3; j++){
-                if (board[i][j] != current_player){
-
-                    possible_row = i;
-                    possible_col = j;
-                }else {
-                    row_correctness++;
-                }
-            }
-            if (row_correctness == 2){
-                updateBoard(possible_row,possible_col,current_player2);
-                return;
+            catch (InputMismatchException ex) {
+                System.out.println("Please enter a number between 1 and " + BOARD_SIZE + ".");
+                s.next();
+                move = -1;
             }
         }
 
-        possible_row = -1;
-        possible_col = -1;
+        mBoard[move-1] = HUMAN_PLAYER;
+    }
 
-        col_correctness = 0;
+    private int checkForWinner() {
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[j][i] != current_player) {
-                    possible_row = j;
-                    possible_col = i;
-                } else {
-                    col_correctness++;
-                }
-            }
-            if (row_correctness == 2) {
-                updateBoard(possible_row, possible_col, current_player2);
-                return;
-            }
+        // Check horizontal wins
+        for (int i = 0; i <= 6; i += 3)	{
+            if (mBoard[i] == HUMAN_PLAYER &&
+                    mBoard[i+1] == HUMAN_PLAYER &&
+                    mBoard[i+2]== HUMAN_PLAYER)
+                return 2;
+            if (mBoard[i] == COMPUTER_PLAYER &&
+                    mBoard[i+1]== COMPUTER_PLAYER &&
+                    mBoard[i+2] == COMPUTER_PLAYER)
+                return 3;
         }
 
-        left_diagonal_count = 0;
-        if (board[0][0] == current_player2){
-            left_diagonal_count++;
-        }else {
-            possible_row = 0;
-            possible_col = 0;
-        }
-        if (board[1][1] == current_player2){
-            left_diagonal_count++;
-        }else {
-            possible_row = 1;
-            possible_col = 1;
-        }
-        if (board[2][2] == current_player2){
-            left_diagonal_count++;
-        }else {
-            possible_row = 2;
-            possible_col = 2;
+        // Check vertical wins
+        for (int i = 0; i <= 2; i++) {
+            if (mBoard[i] == HUMAN_PLAYER &&
+                    mBoard[i+3] == HUMAN_PLAYER &&
+                    mBoard[i+6]== HUMAN_PLAYER)
+                return 2;
+            if (mBoard[i] == COMPUTER_PLAYER &&
+                    mBoard[i+3] == COMPUTER_PLAYER &&
+                    mBoard[i+6]== COMPUTER_PLAYER)
+                return 3;
         }
 
-        if (left_diagonal_count == 2){
-            updateBoard(possible_col,possible_col,current_player2);
-            return;
+        // Check for diagonal wins
+        if ((mBoard[0] == HUMAN_PLAYER &&
+                mBoard[4] == HUMAN_PLAYER &&
+                mBoard[8] == HUMAN_PLAYER) ||
+                (mBoard[2] == HUMAN_PLAYER &&
+                        mBoard[4] == HUMAN_PLAYER &&
+                        mBoard[6] == HUMAN_PLAYER))
+            return 2;
+        if ((mBoard[0] == COMPUTER_PLAYER &&
+                mBoard[4] == COMPUTER_PLAYER &&
+                mBoard[8] == COMPUTER_PLAYER) ||
+                (mBoard[2] == COMPUTER_PLAYER &&
+                        mBoard[4] == COMPUTER_PLAYER &&
+                        mBoard[6] == COMPUTER_PLAYER))
+            return 3;
+
+        // Check for tie
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            // If we find a number, then no one has won yet
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER)
+                return 0;
         }
 
-        right_diagonal_count = 0;
-        if (board[2][0] == current_player2){
-            right_diagonal_count++;
-        }else {
-            possible_row = 2;
-            possible_col = 0;
-        }
-        if (board[1][1] == current_player2){
-            right_diagonal_count++;
-        }else {
-            possible_row = 1;
-            possible_col = 1;
-        }
-        if (board[0][2] == current_player2){
-            right_diagonal_count++;
-        }else {
-            possible_row = 0;
-            possible_col = 2;
-        }
+        // If we make it through the previous loop, all places are taken, so it's a tie
+        return 1;
+    }
 
-        if (right_diagonal_count == 2){
-            updateBoard(possible_col,possible_col,current_player2);
-            return;
-        }
+    private void getComputerMove()
+    {
+        int move;
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                if (board[i][j] != 1 && board[i][j] != 2){
-                    updateBoard(i,j,current_player2);
+        // First see if there's a move O can make to win
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER) {
+                char curr = mBoard[i];
+                mBoard[i] = COMPUTER_PLAYER;
+                if (checkForWinner() == 3) {
+                    System.out.println("Computer is moving to " + (i + 1));
                     return;
                 }
+                else
+                    mBoard[i] = curr;
             }
         }
-        System.out.println("Computer moved");
-    }
 
-    private static boolean checkWinner(int current_player){
-
-        boolean row_check = true;
-        int i;
-        int j;
-        for (i = 0; i< 3; i++){
-            row_check = true;
-            for (j = 0; j < 3; j++){
-                if (board[i][j] != current_player){
-                    row_check = false;
+        // See if there's a move O can make to block X from winning
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER) {
+                char curr = mBoard[i];   // Save the current number
+                mBoard[i] = HUMAN_PLAYER;
+                if (checkForWinner() == 2) {
+                    mBoard[i] = COMPUTER_PLAYER;
+                    System.out.println("Computer is moving to " + (i + 1));
+                    return;
                 }
-            }
-            if (row_check != false && j == 1){
-                System.out.println("Row Strike");
-                WINNER_FOUND = true;
-                return true;
+                else
+                    mBoard[i] = curr;
             }
         }
 
-        for (i = 0; i< 3; i++){
-            row_check = true;
-            for (j = 0; j < 3; j++){
-                if (board[j][i] != current_player){
-                    row_check = false;
-                }
-            }
-            if (row_check != false){
-                System.out.println("Colomn Strike");
-                WINNER_FOUND = true;
-                return true;
-            }
-        }
+        // Generate random move
+        do
+        {
+            move = mRand.nextInt(BOARD_SIZE);
+        } while (mBoard[move] == HUMAN_PLAYER || mBoard[move] == COMPUTER_PLAYER);
 
-        if (board[0][0] == current_player && board[1][1] == current_player && board[2][2] == current_player){
-            WINNER_FOUND = true;
-            return true;
-        }
+        System.out.println("Computer is moving to " + (move + 1));
 
-        for (int row = 0; row < 3; row ++){
-            for (int col = 0; col < 3; col++){
-                if (board[row][col] != 1 || board[row][col] != 2){
-                   return false;
-                }
-            }
-            if (row == 2)
-                is_draw = true;
-            return true;
-        }
-        return false;
+        mBoard[move] = COMPUTER_PLAYER;
     }
 
-    public static void promptInputandSet(int cp){
-
-        System.out.println("Player's draw" + cp + ", enter the row and col from 1 to 3: ");
-        boolean valid_entry = false;
-        do{
-            int row = sc.nextInt() -1;
-            int col = sc.nextInt() -1;
-            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == 0 ){
-                valid_entry = true;
-                updateBoard(row,col,cp);
-            }else {
-                System.out.println("Reenter draw : " + cp + " , enter the row and col from 1 to 3: ");
-            }
-        }while (valid_entry == false);
-    }
-
-    private static void updateBoard(int row, int col, int cp){
-
-        if (cp == 1){
-            board[row][col] = 1;
-        }
-        if (cp == 2){
-            board[row][col] = 2;
-        }
-        printBoard();
-    }
-
-    private static void initializeBoard(){
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                board[i][j] = 0;
-            }
-            System.out.println();
-        }
-    }
-    public static void printBoard(){
-
-        for (int i = 0; i < 3; i++){
-            System.out.print("| ");
-            for (int j = 0; j < 3; j++){
-                System.out.print(board[i][j] + " | ");
-            }
-            System.out.println();
-        }
-    }
 }
 
